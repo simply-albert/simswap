1c5f6f0a5a77ea1373f9b9cacca692b3eeed7910
albertzkiki <albertzkiki@gmail.com>
2024-07-30 23:27:15 +0300

    installation of django, setting up of user and profile functions

---
+++ backend/api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from api.models import User,Profile
+# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_editable = ['verified']
+    list_display = ['verified','full_name','user']
+    list_display_links = ['user']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
